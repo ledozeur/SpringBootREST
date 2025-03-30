f5d24152b7ff622a5652514dc74e18f79b4453ce
ledozeur <git@github.com>
2025-03-30 18:36:22 +0300

    NGINX

--- SpringBootDemo/src/main/java/ru/netology/SpringBootDemo/service/AuthorizationService.java
+++ SpringBootDemo/src/main/java/ru/netology/SpringBootDemo/service/AuthorizationService.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Service
 public class AuthorizationService {
-    UserRepository userRepository;
+    private final UserRepository userRepository;
 
 public AuthorizationService(UserRepository userRepository){
     this.userRepository = userRepository;
